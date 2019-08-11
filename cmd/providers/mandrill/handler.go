3fc7aeef53f8369044398e58d2d925f1f3d11201
Valentino Međimorec <valentino@medjimorec.net>
2019-08-11 18:57:34 +0200

    Use gochimp package for delivery of mails. Replace our struct with pprovided from the package. Added mail templates mapping

--- cmd/providers/mandrill/handler.go
+++ cmd/providers/mandrill/handler.go
@@ -3,59 +3,62 @@ package mandrill
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/bkway/gochimp/mandrill"
 	"github.com/streadway/amqp"
 )
 
 // Definition for Mandrill queue item.
-type Mandrill struct{
+type QueueItem struct {
 	Message struct {
-		Id                      string `json:"id"`
-		Module                  string `json:"module"`
-		Html                    string `json:"html"`
-		Subject                 string `json:"subject"`
-		FromEmail               string `json:"from_email"`
-		FromName                string `json:"from_name"`
-		To                      []struct{
-			Email string `json:"email"`
-			Name string `json:"name, omitempty"`
-			To string `json:"to, omitempty"`
-		} `json:"to"`
-		Headers                 map[string]string `json:"headers"`
-		TrackOpens              bool `json:"track_opens"`
-		TrackClicks             bool `json:"track_clicks"`
-		AutoText                bool `json:"auto_text"`
-		UrlStripQs              bool `json:"url_strip_qs"`
-		BccAddress              string `json:"bcc_address"`
-		Tags                    []string `json:"tags,omitempty"`
-		GoogleAnalyticsDomains  []int `json:"google_analytics_domains,omitempty"`
-		GoogleAnalyticsCampaign string `json:"google_analytics_campaign"`
-		Attachments             []struct{
-			Type string `json:"name"`
-			Name string `json:"name"`
-			Content string `json:"name"`
-		} `json:"attachments, omitempty"`
-		Images             []struct{
-			Type string `json:"name"`
-			Name string `json:"name"`
-			Content string `json:"name"`
-		} `json:"attachments, omitempty"`
-		ViewContentLink         bool `json:"view_content_link"`
-		Metadata                []int `json:"metadata, omitempty"`
+		Id     string `json:"id"`
+		Module string `json:"module, omitempty"`
+		mandrill.Message
 	} `json:"message"`
-
 }
 
 // Process queue item. Unmarshal data to Mandrill struct
 // Preform API calls and set Delivery.Acknowledger status.
-func ProcessItem(Delivery amqp.Delivery, apiKey string) {
-	var data Mandrill
+func ProcessItem(Delivery amqp.Delivery, apiKey string, defaultTemplate string, moduleTemplates map[string]string) bool{
+	var data QueueItem
+
+	//var queueTag = Delivery.DeliveryTag
 
 	err := json.Unmarshal(Delivery.Body, &data)
 	if err != nil {
 		fmt.Println("There was an error:", err)
-		//Delivery.Acknowledger.Reject()
+		//Delivery.Acknowledger.Reject(queueTag, true)
 	}
 
-	fmt.Println(data)
+	if apiKey == "" {
+		fmt.Println("Missing api key")
+	}
+
+	client := mandrill.NewClient(apiKey)
+
+	var templateContent [] mandrill.Variable
+
+	templateContent = append(templateContent, mandrill.Variable{
+		Name:    "body",
+		Content: data.Message.Message.Html,
+	})
+
+	// Specifics for usage with Drupal mandrill module,
+	// but could be reused elsewhere if needed.
+	var templateId = moduleTemplates[data.Message.Id]
+
+	// We don't have specifics. Use default template.
+	if templateId == "" {
+		templateId = defaultTemplate
+	}
+
+	send, err := client.MessagesSendTemplate(templateId, templateContent, &data.Message.Message, true, map[string]string{})
+	if err != nil {
+		fmt.Println("There was an error:", err)
+	}
+
+	fmt.Println(send)
+
+	// Mark message as delivered.
+	return true
 
 }
